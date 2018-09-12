cab89eb5c18ff766b072a6337f37f16ba84de48d
Eric Charnesky <eric.charnesky@careevolution.com>
2018-09-12 00:14:03 -0400

    example updates to lab1

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,4 +1,9 @@
 print('Hello world!')
-print("What is your name?")
-users_name = input()
-print("Hi", users_name, "how are you?")
+users_name = input("What is your name?")
+users_name = "Bob"
+print("Hi", users_name, "how old are you?")
+users_age = int(input())
+print("You might expect to retire in:", 70-users_age, end=' ' )
+print("years")
+users_age += 1
+print("You will be", users_age, "next year")
